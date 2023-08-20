4984ff175d530f70849082cac9f11d1685a09da9
PatrikBeta <141827910+PatrikBeta@users.noreply.github.com>
2023-08-20 21:16:36 +0300

    Add files via upload
    Winaero Tweaker Setup

---
+++ WinaeroSetup.bat
@@ -0,0 +1,7 @@
+@title Installing Winaero Tweaker...
+@timeout /nobreak 8 | break
+set SetupApp=WinaeroTweaker-1.55.0.0-setup.exe
+@timeout /nobreak 8 | break
+%setupapp% /sp+ /silent
+@timeout /nobreak 8 | break
+exit
